a9d43db4d7a648ee6190a74e9484c7441bd84568
javierlemuel <98832519+javierlemuel@users.noreply.github.com>
2024-03-11 20:20:04 -0400

    Small update

--- frontend_model/connectDB.py
+++ frontend_model/connectDB.py
@@ -5,8 +5,8 @@ import pymysql.cursors
 # Use this to access database
 class Dbconnect:
     def __init__(self) -> None:
-        self.connection = pymysql.connect(host='group2-db-ccom4115group2-2b06.a.aivencloud.com',
-                                          db='defaultdb', port=12004, user='avnadmin', password='AVNS_UR3UBFJzMulx_RCqe5P')
+        self.connection = pymysql.connect(host='',
+                                          db='defaultdb', port=, user='avnadmin', password='')
         self.cursor = self.connection.cursor(pymysql.cursors.DictCursor)
 
     def select(self, sql, params=None):
